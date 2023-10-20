f9c73319d85e1dc82c09ec9357f30a4c4c9604e0
Paloma Reynolds <palomap.paula@hotmail.com>
2023-10-20 18:16:09 -0400

    update

--- simon.js
+++ simon.js
@@ -1,82 +1,130 @@
-/*-------------------------------- Constants --------------------------------*/
-const colors = ["green", "red", "yellow", "blue"]
+
+let order = []
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
 
 
-/*-------------------------------- Variables --------------------------------*/
-let count = 1;
-let turnColors = [];
-let color = "";
 
-/*------------------------ Cached Element References ------------------------*/
-// Buttons
-const startBtn = document.getElementById("start")
-const audioGreen = document.getElementById("audio1");
-const audioRed = document.getElementById("audio2");
-const audioYellow = document.getElementById("audio3");
-const audioBlue = document.getElementById("audio4");
-const green = document.getElementsByClassName("green");
-// const playGreen =  document.getElementById("audioGreen")
 
-/*----------------------------- Event Listeners -----------------------------*/
-    startBtn.addEventListener("click", init())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Hints!
+    // define your variables beforehand
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------nothing to do with
+
+
+
+
+// /*-------------------------------- Constants --------------------------------*/
+// const colors = ["Green", "Red", "Yellow", "Blue"]
+
+
+// /*-------------------------------- Variables --------------------------------*/
+// let count = 1;
+// let turnColors = [];
+// let color = "";
+
+// /*------------------------ Cached Element References ------------------------*/
+// // Buttons
+// // const startBtn = document.getElementById("start");
+// const audioGreen = document.getElementById("audio1");
+// const audioRed = document.getElementById("audio2");
+// const audioYellow = document.getElementById("audio3");
+// const audioBlue = document.getElementById("audio4");
+// // const green = document.getElementsByClassName("green");
+// // const topDiv = document.getElementsByClassName("top");
+
+// /*----------------------------- Event Listeners -----------------------------*/
+//     // startBtn.addEventListener("click", init);
+
+//     // audioGreen.addEventListener("click", userColors);
     
 
 
-/*-------------------------------- Functions --------------------------------*/
-function init() {
-        randomColor()
-        // playSound(color)
-}
+// /*-------------------------------- Functions --------------------------------*/
+// // function init() {
+// //         randomColor()
+// //         playSound(turnColors)
+// //         userColors()
+// // }
 
-function randomColor() {
-    let random = Math.floor(Math.random() * colors.length)
-    color = colors[random]
-    turnColors.push(color)
-    console.log(turnColors)
-}
+// // function randomColor() {
+// //     let random = Math.floor(Math.random() * colors.length)
+// //     color = colors[random]
+// //     turnColors.push(color)
+// //     console.log(turnColors)
+// // }
 
-// function playSound(currColor) {
-//     if(currColor === "green") {
-//         audioGreen.play();
+// // function playSound(colorsArr) {
+// //     for(color of colorsArr) {
+// //         document.getElementById(`audio${color}`).play()
+// //     }
+// // }
 
-//     }
-// }
-
-//-----------
-
-
-
-// ----
-// const greenSound = document.getElementById("greenSound");
-// const audioGreen = document.getElementById("audio1");
-
-// greenSound.addEventListener("click", playSound);
-
-// function playSound() {
-//     audioGreen.play()
-//     // try using case switch
-// }
-
-// const clickSound = document.querySelectorAll("audio");
+// // function userColors(e) {
+// //     console.log(e.target)
+// // }
 
 
 
 
-audioGreen.addEventListener("click", function() {
-    document.getElementById("audioGreen").play();
-});
 
-audioRed.addEventListener("click", function() {
-    document.getElementById("audioRed").play();
-});
 
-audioYellow.addEventListener("click", function() {
-    document.getElementById("audioYellow").play();
-});
+// audioGreen.addEventListener("click", function() {
+//     document.getElementById("audioGreen").play();
+// });
 
-audioBlue.addEventListener("click", function() {
-    document.getElementById("audioBlue").play();
-});
+// audioRed.addEventListener("click", function() {
+//     document.getElementById("audioRed").play();
+// });
+
+// audioYellow.addEventListener("click", function() {
+//     document.getElementById("audioYellow").play();
+// });
+
+// audioBlue.addEventListener("click", function() {
+//     document.getElementById("audioBlue").play();
+// });
 
 
 
