e767c56cb5d5453b60b83edd7121e979f8aa4398
Paloma Reynolds <palomap.paula@hotmail.com>
2023-10-21 10:29:46 -0400

    feat: buttons functionalities

--- simon.js
+++ simon.js
@@ -4,7 +4,7 @@ let playerOrder = []; //order that the plays press the color
 let flash; //number of flahses 
 let turn; //what turn we are on
 let good; // true/false weather is the player are doind irght/wrong
-let compTurn; //true/false computers turn platyer turn
+let compTurn; //true/false computers turn player turn
 let intervalId;
 let strict = false; //stric button on or off
 let noise = true; //
@@ -47,4 +47,80 @@ startButton.addEventListener('click', (event) => {
     if (on || win) {
         play();
     }
-})
+});
+
+//reset the variables when th button play is pressed
+function play() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true;
+    //fill up the order array with random numberr that indicates the location of each color
+    for (var i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() * 4) + 1)
+    }
+    compTurn = true;
+    intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+    on = false;
+
+    if (flash === turn) {
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] === 1) one();
+            if (order[flash] === 2) two();
+            if (order[flash] === 3) three();
+            if (order[flash] === 4) four();
+            flash++;
+        }, 200);
+    }
+}
+
+function one() {
+    if (noise) {
+        var audio = document.getElementById("clip1")
+        audio.play();
+    }
+    noise = true;
+    topLeft.style.backgroundColor = "lightgreen"
+}
+
+function two() {
+    if (noise) {
+        var audio = document.getElementById("clip2")
+        audio.play();
+    }
+    noise = true;
+    topLeft.style.backgroundColor = "tomato"
+}
+
+function three() {
+    if (noise) {
+        var audio = document.getElementById("clip3")
+        audio.play();
+    }
+    noise = true;
+    topLeft.style.backgroundColor = "yellow"
+}
+
+function four() {
+    if (noise) {
+        var audio = document.getElementById("clip4")
+        audio.play();
+    }
+    noise = true;
+    topLeft.style.backgroundColor = "lightskyblue"
+}
